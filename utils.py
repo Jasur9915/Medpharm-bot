a8bb0700e1f061f48ff681cdf963ab44e3d85cd9
Jasur9915 <jasurasur405@gmail.com>
2025-07-31 17:02:30 +0500

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-def format_expense(text):
-    try:
-        name, amount = text.rsplit(' ', 1)
-        return f"{name.strip().title()} – {int(amount):,} so'm"
-    except Exception:
-        return "Xatolik! Format: Narsa 10000"
